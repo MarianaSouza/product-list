dfd106103e3be7e47a926e95acf05995d47dc489
Mariana Caldas de Souza <marianacaldassouza@gmail.com>
2021-12-12 00:31:38 -0800

    Add markup and initial styling

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['png.pngtree.com', 'toppng.com', 'www.researchgate.net', 'pngall.com', 'upload.wikimedia.org', 'w7.pngwing.com' ],
+  },
 }
