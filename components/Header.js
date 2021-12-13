6fca30647fdbca8fca77bb7ba8328816dc9ce8d8
Mariana Caldas de Souza <marianacaldassouza@gmail.com>
2021-12-13 10:21:23 -0800

    Fixing warning related to the charset attribute

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
       <title>Edvora Product List</title>
       <meta name="description" content="Product list assessment" />
       <link rel="icon" href="/favicon.ico" />
-      <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
+      <link rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
       <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
     </Head>
   );
